437706cac6a816a897ceefe9393f054729155352
Mridul Sharma <62736480+Mridul001@users.noreply.github.com>
2022-06-15 00:08:06 +0530

    Top View Of A Binary Tree

---
+++ topViewOfBinaryTree.java
@@ -0,0 +1,63 @@
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    class BinaryTreeNode {
+		int val;
+		BinaryTreeNode left;
+		BinaryTreeNode right;
+
+		BinaryTreeNode(int val) {
+			this.val = val;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+************************************************************/
+
+import java.util.*;
+
+public class Solution {
+    
+    public static  class Node{
+        BinaryTreeNode node;
+        int ind;
+        
+        public Node(BinaryTreeNode node, int ind){
+            this.node = node;
+            this.ind = ind;
+        }
+}
+    
+    
+    
+    public static ArrayList<Integer> getTopView(BinaryTreeNode root) {
+         // Write your code here.       
+        
+        Queue<Node> q = new  LinkedList<>();
+        if(root!=null)
+        q.offer(new Node(root, 0));
+        HashMap<Integer, BinaryTreeNode> m = new HashMap<Integer, BinaryTreeNode>(); 
+        int lastInd = 0;
+        while(!q.isEmpty()){
+            Node top = q.poll();
+            BinaryTreeNode node  = top.node;
+            int ind = top.ind;
+            lastInd = Math.min(lastInd, ind);
+            if(m.containsKey(ind)==false)
+            m.put(ind, node);
+            if(node.left!=null)
+                q.offer(new Node(node.left, ind-1));
+            if(node.right!=null)
+                q.offer(new Node(node.right, ind+1));
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(m.containsKey(lastInd)){
+            ans.add(m.get(lastInd).val);
+            lastInd++;
+        }
+        
+        return ans;
+    }
+}
